7f24d1423c3147bf43e99178269d859facf9ba49
Luke Davis <newanswertech@gmail.com>
2019-07-27 04:59:49 -0400

    v0.4-dev series: Mark count now persists through plugin reloads via globalVars (thanks James Scholes for suggestion); now shows in Tools category of NVDA config; added _(...) in appropriate places.

--- addon/globalPlugins/logHelper.py
+++ addon/globalPlugins/logHelper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Log Helper (logHelper.py), version 0.3-dev1
+# Log Helper (logHelper.py), version 0.4-DEV4
 # An NVDA global plugin to make dealing with the NVDA log easier and more efficient.
 
 #    Copyright (C) 2019 Luke Davis <newanswertech@gmail.com>
@@ -19,36 +19,36 @@
 # The lines are modeled after the old Linux Syslog mark lines, and look like this:
 # -- MARK 1 --
 # They are currently logged at the info loglevel.
-# Note: reloading plugins currently resets the number inserted in the log, thus making the numeric part of the mark line somewhat useless.
-# This will be fixed later if possible.
-# Note also that configuration from within NVDA is planned for later; for now see the CONFIG section below.
+# Note that configuration from within NVDA is planned for later; for now see the CONFIG section below.
 
 from __future__ import unicode_literals
-import globalPluginHandler
+from globalCommands import SCRCAT_TOOLS
 from scriptHandler import script
-
-# A global variable to track mark number
-logHelper_markNumber = 0
+import globalPluginHandler, globalVars
 
 class GlobalPlugin (globalPluginHandler.GlobalPlugin):
 
 	# CONFIG
 	# The mark lines can include an arbitrary number of blank lines before or after them, to further help divide your log into sections.
-	newlinesBefore = 0
-	newlinesAfter = 0
+	newlinesBefore = 1
+	newlinesAfter = 1
 	# In case you don't like "-- MARK <number> --" as your text. The number is represented by {0}.
 	markString = "-- MARK {0} --"
+	# END CONFIG
+
+	# Sets the next to be used mark count, initializing it to 1 if not set. Persists across reloads.
+	globalVars.logHelperMarkCount = getattr(globalVars, 'logHelperMarkCount', 1)
 
 	@script(
 		gesture="kb:nvda+shift+F1",
-		description="Inserts a mark line in the log to aid in debugging"
+		description=_("Inserts a mark line in the log to aid in debugging"),
+		category=SCRCAT_TOOLS
 		)
 	def script_logAMarkLine(self, gesture):
-		global logHelper_markNumber
 		import ui
 		from logHandler import log
 		if len(self.markString) == 0: return	# Sanity check
-		logHelper_markNumber += 1
-		message = "{0}{1}{2}".format("\n" * self.newlinesBefore, self.markString.format(logHelper_markNumber), "\n" * self.newlinesAfter)
-		ui.message("Logging mark {0}!".format(logHelper_markNumber))
-		log.info(message)
+		message = "{0}{1}{2}".format("\n" * self.newlinesBefore, self.markString.format(globalVars.logHelperMarkCount), "\n" * self.newlinesAfter)
+		ui.message(_("Logging mark {0}!".format(globalVars.logHelperMarkCount)))
+		log.info(_(message))
+		globalVars.logHelperMarkCount += 1
