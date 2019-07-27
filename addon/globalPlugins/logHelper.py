940709eae3e6ae7884bb4fbbd27665cea3ad1db7
Luke Davis <newanswertech@gmail.com>
2019-07-27 00:43:16 -0400

    v0.1-dev: Initial (basic) working version

---
+++ addon/globalPlugins/logHelper.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Log Helper (logHelper.py), version 0.1-dev.
+# An NVDA global plugin to make dealing with the NVDA log easier and more efficient.
+
+#    Copyright (C) 2019 Luke Davis <newanswertech@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by    the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# This add-on is intended to make debugging tasks easier for developers. If you make frequent use of the NVDA log, this may provide features that help you.
+# Other things are planned, but currently the only implemented feature is:
+# Press NVDA+shift+F1, to insert a sequentially numbered line in the log.
+# This makes it easier to find where you were last, before or after you performed a certain action.
+# The lines are modeled after the old Linux Syslog mark lines, and look like this:
+# -- MARK 1 --
+# They are currently logged at the info loglevel.
+# Note: reloading plugins currently resets the number inserted in the log, thus making the numeric part of the mark line somewhat useless.
+# This will be fixed later if possible.
+# Note also that configuration from within NVDA is planned for later; for now see the CONFIG section below.
+
+from __future__ import unicode_literals
+import globalPluginHandler
+from scriptHandler import script
+
+class GlobalPlugin (globalPluginHandler.GlobalPlugin):
+
+# CONFIG
+	# The mark lines can include an arbitrary number of blank lines after them, to further help divide your log into sections.
+	trailingNewlines = 0
+	# In case you don't like "-- MARK <number> --" as your text. The number is represented by {0}.
+	markString = "-- MARK {0} --"
+
+	@script(
+		gesture="kb:nvda+shift+F1",
+		description="Inserts a mark line in the log to aid in debugging"
+		)
+	def script_logAMarkLine(self, gesture):
+		from logHandler import log
+		import ui
+		markNumber = 0
+		message = self.markString.format(markNumber)
+		if self.trailingNewlines > 0: message += "\n" * self.trailingNewlines
+		if len(message) <= 0: return	# Sanity check
+		ui.message("Log marked!")
+		log.info(message)
---
+++ buildVars.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guidelines.txt
+	# add-on Name, internal for nvda
+	"addon_name" : "logHelper",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary" : _("Log Helper"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description" : _("""This add-on is intended to make debugging tasks easier for developers. If you make frequent use of the NVDA log, this may provide features that help you.
+Other things are planned, but currently the only implemented feature is:
+Press NVDA+shift+F1, to insert a sequentially numbered line in the log. This makes it easier to find where you were last, before or after you performed a certain action. The lines are modeled after the old Linux Syslog mark lines, and look like this:
+-- MARK 1 --
+They are currently logged at the info loglevel."""),
+	# version
+	"addon_version" : "0.1-dev",
+	# Author(s)
+	"addon_author" : u"Luke Davis <newanswertech@gmail.com>",
+	# URL for the add-on documentation support
+	"addon_url" : None,
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	"addon_minimumNVDAVersion" : "2019.1",
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion" : "2019.2",
+	# Add-on update channel (default is stable or None)
+	"addon_updateChannel" : "dev",
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = []
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
