602e7ba225f386a9e405be9510305db50abc72c5
Luke Davis <newanswertech@gmail.com>
2019-07-27 03:09:27 -0400

    v0.3-dev series: code reorganization; re-worked sanity check; Arbitrary new lines are now possible before mark line; mark message spoken on key press now states mark number.

--- addon/globalPlugins/logHelper.py
+++ addon/globalPlugins/logHelper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Log Helper (logHelper.py), version 0.2-dev.
+# Log Helper (logHelper.py), version 0.3-dev1
 # An NVDA global plugin to make dealing with the NVDA log easier and more efficient.
 
 #    Copyright (C) 2019 Luke Davis <newanswertech@gmail.com>
@@ -32,9 +32,10 @@ logHelper_markNumber = 0
 
 class GlobalPlugin (globalPluginHandler.GlobalPlugin):
 
-# CONFIG
-	# The mark lines can include an arbitrary number of blank lines after them, to further help divide your log into sections.
-	trailingNewlines = 0
+	# CONFIG
+	# The mark lines can include an arbitrary number of blank lines before or after them, to further help divide your log into sections.
+	newlinesBefore = 0
+	newlinesAfter = 0
 	# In case you don't like "-- MARK <number> --" as your text. The number is represented by {0}.
 	markString = "-- MARK {0} --"
 
@@ -44,11 +45,10 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
 		)
 	def script_logAMarkLine(self, gesture):
 		global logHelper_markNumber
-		from logHandler import log
 		import ui
+		from logHandler import log
+		if len(self.markString) == 0: return	# Sanity check
 		logHelper_markNumber += 1
-		message = self.markString.format(logHelper_markNumber)
-		if self.trailingNewlines > 0: message += "\n" * self.trailingNewlines
-		if len(message) <= 0: return	# Sanity check
-		ui.message("Log marked!")
+		message = "{0}{1}{2}".format("\n" * self.newlinesBefore, self.markString.format(logHelper_markNumber), "\n" * self.newlinesAfter)
+		ui.message("Logging mark {0}!".format(logHelper_markNumber))
 		log.info(message)
